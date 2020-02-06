3f2899a123e2e421ab2d15a42d1442bec38429d3
mohsen <mohsen@mobilityonegroup.com>
2020-02-06 11:40:56 +0800

    first commit

---
+++ src/main/java/com/example/springbatch/model/User.java
@@ -0,0 +1,67 @@
+package com.example.springbatch.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class User {
+    @Id
+    private Integer id;
+    private String name;
+    private String dept;
+    private String salary;
+    private Date time;
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public User(){}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dept='" + dept + '\'' +
+                ", salary='" + salary + '\'' +
+                '}';
+    }
+}
